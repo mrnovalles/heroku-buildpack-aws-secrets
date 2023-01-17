eda4adfa18900d4bda6bda98fc0e6771c67134d8
Mariano Vallés <mariano.valles@wooga.net>
2023-01-17 22:50:41 +0100

    Add a compile test that works

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 . ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
 
+afterSetUp() {
+  # Populate variables
+  echo "key" > "${ENV_DIR}/AWS_ACCESS_KEY_ID"
+  echo "eu-west-1" > "${ENV_DIR}/AWS_DEFAULT_REGION"
+  echo "secret" > "${ENV_DIR}/AWS_SECRET_ACCESS_KEY"
+  echo "VAR=value" > "${ENV_DIR}/AWS_SECRETS"
+  export test=true
+}
+
 testCompile() {
-  detect
+  compile
   assertCapturedSuccess
 }
 
